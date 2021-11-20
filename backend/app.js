f28a744b9e9be78a4d5bc029ee28191f1f71091e
Yavuz Incedal <yavuzincedalca@gmail.com>
2021-11-20 17:25:23 -0500

    connected to db and received data

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,20 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+
+const { Pool } = require("pg");
+const dbParams = require("./lib/db.js");
+const db = new Pool(dbParams);
+db.connect();
+const dbHelpers = require('./db/helpers/dbHelpers')(db);
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/users', usersRouter(dbHelpers));
 
 module.exports = app;
